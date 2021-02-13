88b9d05d2440e949836ab12834a17b5cc9777153
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 15:14:08 +0700

    Create Dropping duplicates.py

---
+++ 2.Aggregating Data/Dropping duplicates.py
@@ -0,0 +1,13 @@
+# Drop duplicate store/type combinations
+store_types = sales.drop_duplicates(subset=["store", "type"])
+print(store_types.head())
+
+# Drop duplicate store/department combinations
+store_depts = sales.drop_duplicates(subset=["store", "department"])
+print(store_depts.head())
+
+# Subset the rows where is_holiday is True and drop duplicate dates
+holiday_dates = sales[sales["is_holiday"]].drop_duplicates(subset="date")
+
+# Print date col of holiday_dates
+print(holiday_dates["date"])
