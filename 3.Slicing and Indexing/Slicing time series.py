df084a27e272214d8451f4a01b9d3eeaa2a4dbd6
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 15:19:29 +0700

    Create Slicing time series.py

---
+++ 3.Slicing and Indexing/Slicing time series.py
@@ -0,0 +1,12 @@
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+temperatures_bool = temperatures[(temperatures["date"] >= "2010-01-01") & (temperatures["date"] <= "2011-12-31")]
+print(temperatures_bool)
+
+# Set date as an index and sort the index
+temperatures_ind = temperatures.set_index("date").sort_index()
+
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc["2010":"2011"])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc["2010-08":"2011-02"])
