813ca4752909a2692de13d5e9c06a67ee6343017
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 15:17:40 +0700

    Create Subsetting with .loc[].py

---
+++ 3.Slicing and Indexing/Subsetting with .loc[].py
@@ -0,0 +1,18 @@
+# Make a list of cities to subset on
+cities = ["Moscow", "Saint Petersburg"]
+
+# Subset temperatures using square brackets
+print(temperatures[temperatures["city"].isin(cities)])
+
+# Subset temperatures_ind using .loc[]
+print(temperatures_ind.loc[cities])
+
+"Setting multi-level indexes"
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(["country", "city"])
+
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]
+
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
