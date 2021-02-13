486aa8371f3219d324fd7ae44d8dc200d0f71447
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 15:12:38 +0700

    Create Efficient summaries.py

---
+++ 2.Aggregating Data/Efficient summaries.py
@@ -0,0 +1,6 @@
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+    
+# Print IQR of the temperature_c column
+print(sales["temperature_c"].agg(iqr))
