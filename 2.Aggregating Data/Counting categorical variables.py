f66c9a553da890032204acb93169f3792fb56798
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 15:14:28 +0700

    Create Counting categorical variables.py

---
+++ 2.Aggregating Data/Counting categorical variables.py
@@ -0,0 +1,15 @@
+# Count the number of stores of each type
+store_counts = store_types["type"].value_counts()
+print(store_counts)
+
+# Get the proportion of stores of each type
+store_props = store_types["type"].value_counts(normalize=True)
+print(store_props)
+
+# Count the number of each department number and sort
+dept_counts_sorted = store_depts["department"].value_counts(sort=True)
+print(dept_counts_sorted)
+
+# Get the proportion of departments of each number and sort
+dept_props_sorted = store_depts["department"].value_counts(sort=True, normalize=True)
+print(dept_props_sorted)
